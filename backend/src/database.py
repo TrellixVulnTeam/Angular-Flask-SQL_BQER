396363ff21ae2b10361ccab424c08e83b0c31a1c
Paris <parisbielsa@gmail.com>
2021-07-13 21:50:01 +0200

    BugFix

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,4 +1,5 @@
 from genericpath import exists
+from io import TextIOWrapper
 from warnings import catch_warnings
 import sqlalchemy, json, os
 
@@ -7,6 +8,7 @@ try:
     filepath = os.path.realpath(__file__)
     parent = os.path.dirname(filepath)
     filepath = os.path.join(parent, "connection_data.json")
+    file = None
     if(os.path.exists(filepath)):   
         file = open(filepath)
         j = json.load(file)
@@ -17,4 +19,5 @@ try:
 except:
     print("ERROR CONNECTING TO DATABASE")
 finally:
-    file.close()
+    if(file != None):
+        file.close()
